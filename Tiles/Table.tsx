0ca950cf1f27f6117d3c04ff1646e513f0b91de4
BcMaster12 <BcMaster12@users.noreply.github.com>
2019-06-08 22:02:54 -0500

    Well Seymour, i made... dispite your directions
    ah superintendent Chalmers, welcome!
    I hope you're ready for an unforgivable luncheon!

---
+++ Tiles/Table.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="Table" tilewidth="8" tileheight="8" tilecount="24" columns="6">
+ <image source="Table.png" width="48" height="32"/>
+</tileset>
